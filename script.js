9f3396bdd8a8463819c49de2ab75446a99cd9fd8
Ananya-07-git <ananyatiwari3004@gmail.com>
2025-07-17 20:02:07 +0530

    Initial commit: portfolio with HTML, CSS, JS only

---
+++ script.js
@@ -0,0 +1,42 @@
+let tablinks=document.getElementsByClassName("tab-links")
+let tabcontents=document.getElementsByClassName("tab-contents")
+
+function opentab(tabname, event){
+    for ( tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for ( tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+let sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0";
+
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+document.querySelectorAll("#sidemenu a").forEach(link => {
+  link.addEventListener("click", closemenu);
+});
+
+window.addEventListener("scroll", function () {
+  const navMenu = document.querySelector("nav ul");
+  const homeSection = document.querySelector("#header");
+
+  const homeTop = homeSection.getBoundingClientRect().top;
+
+  // If top of home goes out of view, hide the nav
+  if (homeTop < -50) {
+    navMenu.style.right = "-200px";
+  }
+});
+
+
+
+
